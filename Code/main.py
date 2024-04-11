a39f5fd74db67e32afdb18b59493dbeeb1fe006e
GhdEst <Ermopeets53@gmail.com>
2024-04-11 23:22:08 +0300

    player move

--- Code/main.py
+++ Code/main.py
@@ -2,7 +2,7 @@ import pygame as pg
 import sys
 from settings  import *
 from map import *
-
+from player import *
 
 
 class Game:
@@ -10,19 +10,24 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
+
 
     def update(self):
+        self.player.update()
         pg.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
         self.screen.fill('black')
         self.map.draw()
+        self.player.draw
 
     def check_events(self):
          for event in pg.event.get():
--- Code/player.py
+++ Code/player.py
@@ -9,7 +9,49 @@ class Player:
         self.angle = PLAYER_ANGLE
 
     def movement(self):
-        pass
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pg.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pg.K_a]:
+            dx += speed_cos
+            dy += -speed_sin
+        if keys[pg.K_d]:
+            dx += -speed_cos
+            dy += speed_sin
+        
+        self.x += dx
+        self.y += dy
+
+        if keys(pg.K_LEFT):
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys(pg.K_RIGHT):
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        self.angle %= math.tau
+
+    def draw(self):
+        pg.draw.line(self.game.screen, 'blue', (self.x * 100, self.y * 100),
+                     (self.x * 100 + WIDTH * math.cos(self.angle),
+                      self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
 
     def update(self):
         self.movement()
+
+    @property
+    def pos(self):
+        return self.x, self.y
+
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
