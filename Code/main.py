f7bc792f8ec2b45e1c3b98426b77fbeb6b5f863a
GhdEst <Ermopeets53@gmail.com>
2024-04-11 22:36:36 +0300

    done

---
+++ Code/main.py
@@ -0,0 +1,38 @@
+import pygame as pg
+import sys
+from settings  import *
+
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+
+    def new_game(self):
+        pass
+
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+
+    def check_events(self):
+         for event in pg.event.get():
+              if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                   pg.quit()
+                   sys.exit()
+
+    def run(self):
+            while True:
+                 self.check_events()
+                 self.update()
+                 self.draw()
+
+if __name__ == '__main__':
+     game = Game()
+     game.run()
